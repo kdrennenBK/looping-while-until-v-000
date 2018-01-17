739e8791b4d7678bb4cba75e993f703b35b5b941
Kate Drennen <k8ted11@gmail.com>
2018-01-17 03:37:19 +0000

    automatically backed up by learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,8 +1,8 @@
-
-def using_until
-  levitation_force = 6
-  counter = 0
-    until counter >= 6
-      puts "Wingardium Leviosa"#your code here
-      counter += 1
-end
+#
+# def using_until
+#   levitation_force = 6
+#   counter = 0
+#     until counter =10 6
+#       puts "Wingardium Leviosa"#your code here
+#       counter += 1
+# end
