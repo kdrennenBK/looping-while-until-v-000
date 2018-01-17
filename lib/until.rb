edc9320f84725cecc22ae7111d17ea66cbedede6
Kate Drennen <k8ted11@gmail.com>
2018-01-17 03:41:17 +0000

    automatically backed up by learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,8 +1,8 @@
 #
-# def using_until
-#   counter = 6
-#     until counter =10
-#       puts "Wingardium Leviosa"#your code here
-#       counter += 1
-# end
-# end
+def using_until
+  counter = 6
+    until counter =10
+      puts "Wingardium Leviosa"#your code here
+      counter += 1
+end
+end
